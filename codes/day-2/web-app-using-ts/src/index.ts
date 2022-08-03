b8dd984b2edfe97bde9c19e1eab02e67f031e492
Joydip <joydip.mondal@gmail.com>
2022-08-03 10:38:56 +0530

    updated app for webpack

--- codes/day-2/web-app-using-ts/src/index.ts
+++ codes/day-2/web-app-using-ts/src/index.ts
@@ -1,14 +1,5 @@
-//call thsoe functions from here and pass two values
-
-//common js format
-// var utilityModuleExports = require('./utility') //{addFn:add, subtractFn:subract}
-//var { add, subtract } = require('./utility') //{addFn:add, subtractFn:subract}
-
-//ES6 import
 import { add, multiply, subtract } from "./utility";
 
-// var addResult = utilityModuleExports.add(10, 20)
-// var subResult = utilityModuleExports.subtract(10, 4)
 const addResult = add(10, 20)
 const subResult = subtract(10, 4)
 const multiResult = multiply(10, 4)
@@ -16,24 +7,10 @@ console.log(addResult, subResult)
 
 const spanEle = <HTMLSpanElement>document.getElementById('resultSpan');
 spanEle.innerText = multiResult.toString()
-//console.log(module)
-console.log('hello')
-
-//function expression
-// const call = (name: string) => {
-//     alert(name)
-// }
-
 
 const buttonEle = <HTMLButtonElement>document.getElementById('btnClick')
 buttonEle.addEventListener(
     'click',
-    (e) => {
-        console.log(e)
+    () => {
         console.log((<HTMLInputElement>document.getElementById('txtName')).value)
     })
-
-//function declaration
-// function call(name: string) {
-//     alert(name)
-// }
--- codes/day-2/web-app-using-ts/src/utility.ts
+++ codes/day-2/web-app-using-ts/src/utility.ts
@@ -1,5 +1,3 @@
-//create two functions to add two numbers and subtract two numbers
-//ES6 format to export
 export function add(a: number, b: number): number {
     return (a + b);
 }
@@ -10,11 +8,3 @@ export const subtract = (a: number, b: number): number => {
 export const multiply = (a: number, b: number): number => {
     return (a * b);
 }
-
-// module.exports = {
-//     // addFn: add,
-//     // subtractFn: subtract
-//     add, //ad:add
-//     subtract //subtract:subtract
-// }
-//any
