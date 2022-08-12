3e7b59dd212d6f92ea082dfefb1740d934dea506
Joydip <joydip.mondal@gmail.com>
2022-08-12 11:35:29 +0530

    login component updated with reactive form code

--- codes/day-9/product-management-system-app-v6/src/app/auth/components/login/login.component.ts
+++ codes/day-9/product-management-system-app-v6/src/app/auth/components/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +8,49 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  //1. creating individual formcontrol instances
+  // username: FormControl = new FormControl('',[Validators.required, Validators.email])
+  // password: FormControl = new FormControl('', [Validators.required, Validators.minLength(6)])
 
-  ngOnInit(): void {
+  //2. creating instance of formgroup with a collection of instances of formcontrol
+
+  // loginFrm: FormGroup = new FormGroup({
+  //   username: new FormControl('',[Validators.required, Validators.email]),
+  //   password: new FormControl('',[Validators.required, Validators.minLength(6)])
+  // })
+
+
+  loginFrm?: FormGroup;
+  constructor(private builder: FormBuilder) {
+    //3. creating iinstnce or formgroup with a collection of formcotntrols using FormBuilder service
+    this.loginFrm = this.builder.group({
+      'username': ['', [Validators.required, Validators.email]],
+      'password': ['', [Validators.required, Validators.minLength(6)]]
+    })
   }
 
+  get username() {
+    return this.loginFrm?.get('username')
+  }
+
+  get password() {
+    return this.loginFrm?.get('password')
+  }
+
+  ngOnInit(): void {
+    //let username = this.builder.control('')
+    // this.loginFrm = this.builder.group({
+    //   username: [''],
+    //   password: ['']
+    // })
+  }
+
+  submitUser() {
+    // console.log(this.username.value, this.password.value)
+    //console.log(this.loginFrm.value)
+    // console.log(this.loginFrm.get('username')?.value)
+    // console.log(this.loginFrm.get('password')?.value)
+    console.log(this.loginFrm?.controls['username'].value)
+    console.log(this.loginFrm?.controls['password'].value)
+  }
 }
